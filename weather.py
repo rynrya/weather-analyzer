9b114ed30bc4ef6b25cadbd338d3dccd76e4daca
rynrya <aryanaryalko@gmail.com>
2026-04-03 20:01:57 +0530

    removed the matplotlib import and the plotting code, as it was not being used in the final version of the weather application. This helps to clean up the code and reduce unnecessary dependencies.

--- weather.py
+++ weather.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import plotly.express as px
